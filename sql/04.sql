5f891e06d2cb176c98ad7973d0400ad9ada28691
Sam Bogen <samcbogen@gmail.com>
2022-02-22 20:46:34 -0800

    fix another typo

--- sql/04.sql
+++ sql/04.sql
@@ -5,10 +5,11 @@
  * It's possible to solve this problem both with and without subqueries.
  */
 
-select f.title from film f
+select f.title
+from film f
 inner join inventory i on f.film_id = i.film_id
 inner join rental r on r.inventory_id = i.inventory_id
-where r.customer_id = 1 
-group by f.title 
-having count(r.coustomer_id) > 1
+where r.customer_id = 1
+group by f.title
+having count(r.customer_id) > 1
 order by title;
